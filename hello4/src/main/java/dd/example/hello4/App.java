137125f55789bd0dbf594fd7f0a334b6834585c2
root <root@andrea-K53TK>
2020-05-27 13:58:30 +0200

    prova

---
+++ hello4/src/main/java/dd/example/hello4/App.java
@@ -0,0 +1,13 @@
+package dd.example.hello4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
